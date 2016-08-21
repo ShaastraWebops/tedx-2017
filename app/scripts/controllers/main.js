acfa665d6e02a32024eea138fb94fadb1b5b6c97
Minu Aswanth <minuapple@gmail.com>
2016-08-21 22:20:00 +0530

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tedx2017App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tedx2017App
+ */
+angular.module('tedx2017App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
